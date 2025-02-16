fce27f34f8f5e834cb8e82463d7182d51e49ffcf
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-16 02:05:44 -0400

    refactor, use `lib4bin` hooks

--- pavucontrol-qt-appimage.sh
+++ pavucontrol-qt-appimage.sh
@@ -10,10 +10,9 @@ UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 
 # Prepare AppDir
-mkdir -p ./"$APP"/AppDir/usr/share/applications
-cd ./"$APP"/AppDir
+mkdir -p ./AppDir
+cd ./AppDir
 
-cp /usr/share/applications/pavucontrol-qt.desktop ./usr/share/applications
 cp /usr/share/applications/pavucontrol-qt.desktop ./
 wget "$ICON" -O multimedia-volume-control.svg
 ln -s ./multimedia-volume-control.svg ./.DirIcon
@@ -30,23 +29,15 @@ chmod +x ./AppRun
 # ADD LIBRARIES
 wget "$LIB4BN" -O ./lib4bin
 chmod +x ./lib4bin
-./lib4bin -p -w "$(command -v pavucontrol-qt)"
-rm -f ./lib4bin
-
-# DELOY QT
-mkdir -p ./shared/lib/qt6/plugins
-cp -r /usr/lib/qt6/plugins/iconengines       ./shared/lib/qt6/plugins
-cp -r /usr/lib/qt6/plugins/imageformats      ./shared/lib/qt6/plugins
-cp -r /usr/lib/qt6/plugins/platforms         ./shared/lib/qt6/plugins
-cp -r /usr/lib/qt6/plugins/platformthemes    ./shared/lib/qt6/plugins
-cp -r /usr/lib/qt6/plugins/styles            ./shared/lib/qt6/plugins
-cp -r /usr/lib/qt6/plugins/xcbglintegrations ./shared/lib/qt6/plugins
-cp -r /usr/lib/qt6/plugins/wayland-*         ./shared/lib/qt6/plugins
-
-ldd ./shared/lib/qt6/plugins/*/* 2>/dev/null \
-  | awk -F"[> ]" '{print $4}' | xargs -I {} cp -nv {} ./shared/lib || true
-
-find ./shared/lib -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+xvfb-run -a -- ./lib4bin -p -v -e -s -k \
+	"$(command -v pavucontrol-qt)" \
+	/usr/lib/qt6/plugins/iconengines/* \
+	/usr/lib/qt6/plugins/imageformats/* \
+	/usr/lib/qt6/plugins/platforms/* \
+	/usr/lib/qt6/plugins/platformthemes/* \
+	/usr/lib/qt6/plugins/styles/* \
+	/usr/lib/qt6/plugins/xcbglintegrations/* \
+	/usr/lib/qt6/plugins/wayland-*/*
 
 ./sharun -g # makes sharun generate the lib.path file
 VERSION=$(./AppRun --version 2>/dev/null | awk 'NR==1 {print $2; exit}')
