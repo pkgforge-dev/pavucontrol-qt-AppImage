038dae2a1c29d7d5ae1fe9273e04803ad2325e2e
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-09 18:29:14 -0400

    simplify deployment with wrapper script

--- pavucontrol-qt-appimage.sh
+++ pavucontrol-qt-appimage.sh
@@ -1,55 +1,34 @@
 #!/bin/sh
 
-set -eu
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-APP=pavucontrol-qt
+set -ex
+
+ARCH="$(uname -m)"
+
 ICON="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/yast-sound.svg"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
 URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+APPRUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/AppRun-Qt"
+
+VERSION=$(pacman -Q pavucontrol-qt | awk 'NR==1 {print $2; exit}')
+echo "$VERSION" > ~/version
 
 # Prepare AppDir
 mkdir -p ./AppDir
-cd ./AppDir
-
-cp /usr/share/applications/pavucontrol-qt.desktop ./
-wget "$ICON" -O multimedia-volume-control.svg
-ln -s ./multimedia-volume-control.svg ./.DirIcon
-
-cat >> ./AppRun << 'EOF'
-#!/bin/sh
-CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-[ -f "$APPIMAGE".stylesheet ] && APPIMAGE_QT_THEME="$APPIMAGE.stylesheet"
-[ -f "$APPIMAGE_QT_THEME" ] && set -- "$@" "-stylesheet" "$APPIMAGE_QT_THEME"
-"$CURRENTDIR/bin/pavucontrol-qt" "$@"
-EOF
-chmod +x ./AppRun
+cp -v /usr/share/applications/pavucontrol-qt.desktop ./AppDir
+wget "$ICON" -O ./AppDir/multimedia-volume-control.svg
+cp -v ./AppDir/multimedia-volume-control.svg ./AppDir/.DirIcon
 
 # ADD LIBRARIES
-wget "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -a -- ./lib4bin -p -v -e -s -k \
-	"$(command -v pavucontrol-qt)" \
-	/usr/lib/qt6/plugins/iconengines/* \
-	/usr/lib/qt6/plugins/imageformats/* \
-	/usr/lib/qt6/plugins/platforms/* \
-	/usr/lib/qt6/plugins/platformthemes/* \
-	/usr/lib/qt6/plugins/styles/* \
-	/usr/lib/qt6/plugins/xcbglintegrations/* \
-	/usr/lib/qt6/plugins/wayland-*/*
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun "$(command -v pavucontrol-qt)"
 
-./sharun -g # makes sharun generate the lib.path file
-VERSION=$(./AppRun --version 2>/dev/null | awk 'NR==1 {print $2; exit}')
-if [ -z "$VERSION" ]; then
-	VERSION=$(pacman -Q pavucontrol-qt | awk 'NR==1 {print $2; exit}')
-fi
-export VERSION
-echo "$VERSION" > ~/version
+wget --retry-connrefused --tries=30 "$APPRUN" -O ./AppDir/AppRun
+chmod +x ./AppDir/AppRun
 
 # MAKE APPIAMGE WITH URUNTIME
-cd ..
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime
 wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O ./uruntime-lite
 chmod +x ./uruntime*
@@ -64,7 +43,7 @@ echo "Generating AppImage..."
 	--no-history --no-create-timestamp \
 	--compression zstd:level=22 -S26 -B8 \
 	--header uruntime-lite \
-	-i ./AppDir -o ./"$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
+	-i ./AppDir -o ./pavucontrol-qt-"$VERSION"-anylinux-"$ARCH".AppImage
 
 echo "Generating zsync file..."
 zsyncmake *.AppImage -u *.AppImage
