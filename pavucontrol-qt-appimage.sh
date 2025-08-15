000fdfc5a87413304b493702a348d86d61d33294
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-15 01:44:59 -0400

    use `uruntime2appimage`

--- pavucontrol-qt-appimage.sh
+++ pavucontrol-qt-appimage.sh
@@ -1,51 +1,26 @@
 #!/bin/sh
 
-set -ex
+set -eUx
 
 ARCH="$(uname -m)"
-
-ICON="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/yast-sound.svg"
-URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
-URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
-UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-APPRUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/AppRun-Qt"
 
 VERSION=$(pacman -Q pavucontrol-qt | awk 'NR==1 {print $2; exit}')
-echo "$VERSION" > ~/version
+[ -n "$VERSION" ] && echo "$VERSION" > ~/version
 
-# Prepare AppDir
-mkdir -p ./AppDir
-cp -v /usr/share/applications/pavucontrol-qt.desktop ./AppDir
-wget "$ICON" -O ./AppDir/multimedia-volume-control.svg
-cp -v ./AppDir/multimedia-volume-control.svg ./AppDir/.DirIcon
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=pavucontrol-qt-"$VERSION"-anylinux-"$ARCH".AppImage
+export ICON="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/yast-sound.svg"
+export DESKTOP=/usr/share/applications/pavucontrol-qt.desktop
 
 # ADD LIBRARIES
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun "$(command -v pavucontrol-qt)"
 
-wget --retry-connrefused --tries=30 "$APPRUN" -O ./AppDir/AppRun
-chmod +x ./AppDir/AppRun
-
-# MAKE APPIAMGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime
-wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O ./uruntime-lite
-chmod +x ./uruntime*
-
-# Add udpate info to runtime
-echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime-lite --appimage-addupdinfo "$UPINFO"
-
-echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime-lite \
-	-i ./AppDir -o ./pavucontrol-qt-"$VERSION"-anylinux-"$ARCH".AppImage
-
-echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
-
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
 echo "All Done!"
