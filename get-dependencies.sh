cb83c20f18389a908a1b4d23a5fc899df2df6ab0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-07 07:26:41 -0400

    update to template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,28 +1,13 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
-echo "Installing build dependencies..."
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel            \
-	curl                  \
-	git                   \
-	hicolor-icon-theme    \
-	libxtst               \
-	pavucontrol-qt        \
-	pipewire-audio        \
-	pulseaudio            \
-	pulseaudio-alsa       \
-	qt6ct                 \
-	wget                  \
-	xorg-server-xvfb      \
-	zsync
+pacman -Syu --noconfirm pavucontrol-qt qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh qt6-base-mini libxml2-mini opus-mini
+get-debloated-pkgs --add-common --prefer-nano
