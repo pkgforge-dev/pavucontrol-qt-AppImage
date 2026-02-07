1b0e1d31b6190409d5fd697cbc2c84e8f1b6d615
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-07 07:29:46 -0400

    space

--- pavucontrol-qt-appimage.sh
+++ pavucontrol-qt-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q pavucontrol-qt| awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q pavucontrol-qt | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
